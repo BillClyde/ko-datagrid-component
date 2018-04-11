46a07235b60027252f37680e69f22d32ec3decf9
William Clyde <bcly461@ecy.wa.gov>
2018-04-11 11:38:00 -0700

    Pager functionality is complete

--- src/components/datagrid-pager.js
+++ src/components/datagrid-pager.js
@@ -11,35 +11,48 @@ ko.components.register("pager" , {
     self.pagerClasses = ko.observable("pagination " + (self.tableClasses || ""));
     self.pageItems = ko.observableArray([]);
     self.currentPageIndex = ko.observable(0);
+
     self.previousPage = function () {
       var currIndex = self.currentPageIndex() - 1;
       self.currentPageIndex(currIndex);
       self.pageItems(self.itemsOnCurrentPage());
     }
+
     self.currentPageIndex.subscribe(function (newIdx) {
       self.pageItems(self.itemsOnCurrentPage());
     });
+
     self.nextPage = function () {
       var currIndex = self.currentPageIndex() + 1;
       self.currentPageIndex(currIndex);
       self.pageItems(self.itemsOnCurrentPage());
     }
-    self.setPageSize = function (data) {
-      self.pageSize(data);
+
+    self.setPageSize = function (size) {
+      self.pageSize(size);
       self.pageItems(self.itemsOnCurrentPage());
-    };
+      if (self.currentPageIndex() > self.maxPageIndex()){
+        self.currentPageIndex(self.maxPageIndex());
+      }
+    }
+
     self.minRange = ko.pureComputed(function () {
       var min;
-      if ((self.currentPageIndex() + 4) > self.maxPageIndex()) {
-        console.log("Reaching Max");
+      if (self.currentPageIndex() >= self.maxPageIndex() - 3) {
+        min = self.maxPageIndex() - 4
+      } else {
+        min = (self.currentPageIndex() - 2) < 0 ? 0 : (self.currentPageIndex() - 2);
       }
-      min = (self.currentPageIndex() - 2) < 0 ? 0 : (self.currentPageIndex() - 2);
       return min
     });
 
     self.maxRange = ko.pureComputed(function () {
       var max;
-      max = self.currentPageIndex() === 3 ? self.minRange() + 3 : self.minRange() + 4;
+      if (self.currentPageIndex() >= self.maxPageIndex() - 4){
+        max = self.currentPageIndex() === self.maxPageIndex() - 4 ? self.maxPageIndex() - 2 : self.maxPageIndex();
+      } else {
+        max = self.currentPageIndex() === 3 ? self.minRange() + 3 : self.minRange() + 4;
+      }
       return max;
     });
 
